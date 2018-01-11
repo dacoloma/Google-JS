3f35625bef379abd5233b9eab7482681dbf1f22f
D.Coloma <dnl.coloma@gmail.coma>
2018-01-11 16:12:59 +0100

    warm up

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,21 @@
+//Mes bouttons
 let monBouton = document.querySelector('button');
+let searchButton = document.getElementById("search");
+let luckButton = document.getElementById("luck");
+
 let middle = document.getElementById("middle_field");
 
 monBouton.onclick = function() {
+    middle.style.display="flex";
+    monBouton.style.display="none";
+}
+
+searchBouton.onclick = function() {
+
+    
+}
+
+luckBouton.onclick = function() {
     middle.style.display="flex";
     monBouton.style.display="none";
 }
